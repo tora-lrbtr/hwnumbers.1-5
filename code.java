1107b842c6ac54e6451a165b40cc31822889f8f9
tora <102689338+tora-lrbtr@users.noreply.github.com>
2022-04-18 01:05:28 +0600

    update
    gone code

---
+++ code.java
@@ -0,0 +1,10 @@
+public class code {
+    public static void main(String[] args) {
+        int i = 5, n = 1;
+        do {
+            System.out.println(i);
+            i --; }
+        while ( i >= n);
+        }
+    }
+
